42c229dbe67f0060cbea569027f1546d8581d266
pmapelli <pedromapelli@MacBook-Air-de-Pedro.local>
2020-04-11 11:50:53 -0300

    feat: Create login with email and password

--- src/app/controllers/restaurantController.js
+++ src/app/controllers/restaurantController.js
@@ -10,13 +10,21 @@ module.exports = {
   },
   async create(request, response) {
     try {
-      const { name, email, whatsapp, password, city, uf } = request.body;
+      const {
+        name,
+        email,
+        whatsapp,
+        address,
+        city,
+        uf,
+        password,
+      } = request.body;
 
       const id = generateUniqueId();
 
-      bcrypt.hash(password, 8, async (err, result) => {
+      bcrypt.hash(password, 8, async (err, password) => {
         if (err) {
-          res.json({ error: "Server Error" });
+          response.json({ error: "Server Error" });
         }
         await connection("restaurants").insert({
           id,
@@ -24,6 +32,7 @@ module.exports = {
           email,
           password,
           whatsapp,
+          address,
           city,
           uf,
         });
@@ -31,7 +40,7 @@ module.exports = {
 
       return response.json(id);
     } catch (err) {
-      return res.status(400).send({ ßerror: "Registration failed" });
+      return response.status(400).send({ error: "Registration failed" });
     }
   },
 };
--- src/database/migrations/20200327094806_create_restaurants.js
+++ src/database/migrations/20200327094806_create_restaurants.js
@@ -1,14 +1,16 @@
-
-exports.up = function(knex) {
-  return knex.schema.createTable('restaurants', function(table){
-    table.string('id').primary();
-    table.string('name').notNullable();
-    table.string('email').notNullable();
-    table.string('whatsapp').notNullable();
-    table.string('address').notNullable();
+exports.up = function (knex) {
+  return knex.schema.createTable("restaurants", function (table) {
+    table.string("id").primary();
+    table.string("name").notNullable();
+    table.string("email").notNullable();
+    table.string("whatsapp").notNullable();
+    table.string("address").notNullable();
+    table.string("city").notNullable();
+    table.string("uf").notNullable();
+    table.string("password").notNullable();
   });
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTable('restaurants');
+exports.down = function (knex) {
+  return knex.schema.dropTable("restaurants");
 };
